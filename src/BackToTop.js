0753afd368d55ea692dfa522ebdb4e9fb3d373bc
joaovitorf4 <joaovitorfod5@gmail.com>
2024-07-01 10:34:32 -0300

    Back to Top Button

---
+++ src/BackToTop.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import './BackToTop.css';
+
+const BackToTop = () => {
+    const [isVisible, setIsVisible] = useState(false);
+
+    const toggleVisibility = () => {
+        if (window.scrollY > 100) {
+            setIsVisible(true);
+        } else {
+            setIsVisible(false);
+        }
+    };
+
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', toggleVisibility);
+
+        return () => {
+            window.removeEventListener('scroll', toggleVisibility);
+        };
+    }, []);
+
+    return (
+        <div className="back-to-top">
+            <button
+                className={isVisible ? 'show' : ''}
+                onClick={scrollToTop}
+                title="Go to top"
+            >
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
+                    <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+                </svg>
+            </button>
+        </div>
+    );
+};
+
+export default BackToTop;
