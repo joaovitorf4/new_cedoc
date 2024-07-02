c90dc337bc3192588258b7104d0a1ed217f117c1
joaovitorf4 <joaovitorfod5@gmail.com>
2024-07-02 10:32:47 -0300

    some restyling

--- src/BackToTop.js
+++ src/BackToTop.js
@@ -34,8 +34,8 @@ const BackToTop = () => {
                 onClick={scrollToTop}
                 title="Go to top"
             >
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-up" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-up" viewBox="0 0 16 16">
+                    <path fillRule="evenodd" d="M7.646 4.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1-.708.708L8 5.707l-5.646 5.647a.5.5 0 0 1-.708-.708z"/>
                 </svg>
             </button>
         </div>
