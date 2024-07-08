551977ab33333c723c133a3b76f9591b814d0100
joaovitorf4 <joaovitorfod5@gmail.com>
2024-07-08 10:23:25 -0300

    new font

--- src/styled-components/Title.js
+++ src/styled-components/Title.js
@@ -5,13 +5,12 @@ import styled from 'styled-components';
 const Title = styled.h1`
     font-size: 8vh;
     max-width: 100vw;
-    font-family: "Euclid";
+    font-family: "Chakra Petch", sans-serif;
+    font-weight: 600;
 
     @media (max-aspect-ratio: 1.25/1){
         text-align: center;
         font-size: 7vh;
-        max-width: 100vw;
-        font-family: "Euclid";
     }
 `;
 
