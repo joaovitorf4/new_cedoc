a18d97e5213719211b3fb0447877cfbce4332573
joaovitorf4 <joaovitorfod5@gmail.com>
2024-07-23 09:40:36 -0300

    exporting function

--- src/pages/BackToTop.js
+++ src/pages/BackToTop.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import './BackToTop.css';
 
+export const scrollToTop = () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+};
+
 const BackToTop = () => {
     const [isVisible, setIsVisible] = useState(false);
 
@@ -10,13 +17,6 @@ const BackToTop = () => {
         } else {
             setIsVisible(false);
         }
-    };
-
-    const scrollToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: 'smooth'
-        });
     };
 
     useEffect(() => {
