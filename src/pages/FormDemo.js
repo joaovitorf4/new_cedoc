08f867bec4eaef0441eee069239ed1d89d900a5a
joaovitorf4 <joaovitorfod5@gmail.com>
2024-08-02 13:34:02 -0300

    bg bug

--- src/pages/FormDemo.js
+++ src/pages/FormDemo.js
@@ -11,6 +11,8 @@ import { scrollToTop } from './BackToTop';
 function FormDemo({bgImg = 'url("https://static.vecteezy.com/ti/vetor-gratis/p1/19938741-premio-fundo-projeto-com-onda-padronizar-dentro-cinzento-cor-vetor.jpg")'}) {
   const style = {
     backgroundImage: bgImg,
+    backgroundRepeat: 'no-repeat',
+    backgroundSize: '100% 100%',
   };
   const [inProp, setInProp] = useState(false);
   const [selectedOption, setSelectedOption] = useState(null);
