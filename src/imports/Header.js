84fab5c0fca1334d7f51917e42243db2bd6ff1d6
joaovitorf4 <joaovitorfod5@gmail.com>
2024-08-07 10:15:05 -0300

    menu visibility bug fix

--- src/imports/Header.js
+++ src/imports/Header.js
@@ -1,9 +1,8 @@
 import './Header.css';
-import logo from '../imgs/logo_cedoc.png'
-import React, { useState } from 'react';
+import logo from '../imgs/logo_cedoc.png';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { CSSTransition } from 'react-transition-group';
-import { useEffect } from 'react';
 
 const linkStyle = {
   textDecoration: 'none',
@@ -16,6 +15,8 @@ const hoverStyle = {
 
 function App() {
   const [inProp, setInProp] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isVisible, setIsVisible] = useState(false);
 
   useEffect(() => {
     setTimeout(() => {
@@ -23,32 +24,39 @@ function App() {
     }, 100);
   }, []);
 
-  const [menuOpen, setMenuOpen] = useState(false);
-  const [isVisible, setIsVisible] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (menuOpen) {
+        // Keep header visible when menu is open
+        setIsVisible(true);
+      } else {
+        // Set visibility based on scroll position when menu is closed
+        setIsVisible(window.scrollY > 10);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    handleScroll(); // Update visibility on mount
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, [menuOpen]);
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
-    setIsVisible(true);
+    setMenuOpen(prevMenuOpen => {
+      const newMenuOpen = !prevMenuOpen;
+      // Set visibility to true when the menu is opened
+      if (newMenuOpen) {
+        setIsVisible(true);
+      }
+      return newMenuOpen;
+    });
   };
 
   const closeMenu = () => {
     setMenuOpen(false);
   };
-    const toggleVisibility = () => {
-        if (window.scrollY > 10) {
-            setIsVisible(true);
-        } else {
-            setIsVisible(false);
-        }
-    };
-
-  useEffect(() => {
-    window.addEventListener('scroll', toggleVisibility);
-
-    return () => {
-        window.removeEventListener('scroll', toggleVisibility);
-    };
-  }, []);
 
   return (
     <div className="Header">
@@ -58,27 +66,27 @@ function App() {
         classNames="fade"
         unmountOnExit
       >
-      <header className={isVisible ? 'show' : ''}>
-        <Link to={"/"} onClick={closeMenu}>
-          <img src={logo} alt="Logo" />
-        </Link>
-        <nav>
-          <div className="menu-toggle" onClick={toggleMenu}>
-            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
-              <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-            </svg>
-          </div>
-          <ul className={`menu ${menuOpen ? 'active' : ''}`}>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/"} onClick={closeMenu}>Home</Link></li>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/services"} onClick={closeMenu}>Serviços e Soluções</Link></li>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/clients"} onClick={closeMenu}>Clientes</Link></li>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/about"} onClick={closeMenu}>Sobre nós</Link></li>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/contact"} onClick={closeMenu}>Suporte e Contato</Link></li>
-            <li><Link style={linkStyle} id="demo" activestyle={hoverStyle} to={"/formdemo"} onClick={closeMenu}>Solicite uma demonstração</Link></li>
-            <li><Link style={linkStyle} id="client" activestyle={hoverStyle} to={"/auth"} onClick={closeMenu}>Área do cliente</Link></li>
-          </ul>
-        </nav>
-      </header>
+        <header className={isVisible ? 'show' : ''}>
+          <Link to={"/"} onClick={closeMenu}>
+            <img src={logo} alt="Logo" />
+          </Link>
+          <nav>
+            <div className="menu-toggle" onClick={toggleMenu}>
+              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-list" viewBox="0 0 16 16">
+                <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
+              </svg>
+            </div>
+            <ul className={`menu ${menuOpen ? 'active' : ''}`}>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/"} onClick={closeMenu}>Home</Link></li>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/services"} onClick={closeMenu}>Serviços e Soluções</Link></li>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/clients"} onClick={closeMenu}>Clientes</Link></li>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/about"} onClick={closeMenu}>Sobre nós</Link></li>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/contact"} onClick={closeMenu}>Suporte e Contato</Link></li>
+              <li><Link style={linkStyle} id="demo" activestyle={hoverStyle} to={"/formdemo"} onClick={closeMenu}>Solicite uma demonstração</Link></li>
+              <li><Link style={linkStyle} id="client" activestyle={hoverStyle} to={"/auth"} onClick={closeMenu}>Área do cliente</Link></li>
+            </ul>
+          </nav>
+        </header>
       </CSSTransition>
     </div>
   );
