75eaf3c938030f899cecfc28a5032478a5b51150
joaovitorf4 <joaovitorfod5@gmail.com>
2024-08-20 13:14:20 -0300

    redesign footer

--- src/imports/Footer.js
+++ src/imports/Footer.js
@@ -36,20 +36,25 @@ function App() {
       <footer>
         <div id='footer-content'>
           <section id='first-sec'>
-          <Link style={linkStyle} activestyle={hoverStyle} to={"/contact"}><Title2Footer>Contato</Title2Footer></Link>
+            <Title2Footer>Contato</Title2Footer>
             <ul>
-              <li className='location'><p>Unidade de Administrativa</p><a href={cedocLocationAdm} target='_blank' rel='noreferrer'>Avenida Portugal, 399 Jardim Atlântico - Belo Horizonte - MG</a></li>
-              <li className='location'><p>Unidade de Armazenamento</p><a href={cedocLocationArm} target='_blank' rel='noreferrer'>Rua Alcobaça, 831 São Francisco - Belo Horizonte - MG</a></li>
-              <li id='email-contact'><a href={comercialEmail} target='_blank' rel='noreferrer'>comercial@cedoc.net.br</a></li>
-              <li id='number'><a href={geralTel2} target='_blank' rel='noreferrer'>(31) 99990-00722</a></li>
+            <li id='email-contact'><a href={comercialEmail} target='_blank' rel='noreferrer'>comercial@cedoc.net.br</a></li>
+            <li id='number'><a href={geralTel2} target='_blank' rel='noreferrer'>(31) 99990-00722</a></li>
+            </ul>
+          </section>
+          <section id='first-sec'>
+            <Title2Footer>Localização</Title2Footer>
+            <ul>
+            <li className='location'><p>Unidade de Administrativa</p><a href={cedocLocationAdm} target='_blank' rel='noreferrer'>Avenida Portugal, 399 Jardim Atlântico - Belo Horizonte - MG</a></li>
+            <li className='location'><p>Unidade de Armazenamento</p><a href={cedocLocationArm} target='_blank' rel='noreferrer'>Rua Alcobaça, 831 São Francisco - Belo Horizonte - MG</a></li>
             </ul>
           </section>
           <section id='second-sec'>
             <Title2Footer>Páginas</Title2Footer>
             <ul>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/services"}>Serviços e Soluções</Link></li>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/clients"}>Clientes</Link></li>
-            <li><Link style={linkStyle} activestyle={hoverStyle} to={"/about"}>Sobre nós</Link></li>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/services"}>Serviços e Soluções</Link></li>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/clients"}>Clientes</Link></li>
+              <li><Link style={linkStyle} activestyle={hoverStyle} to={"/about"}>Sobre nós</Link></li>
             </ul>
           </section>
         </div>
