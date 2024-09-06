165e979ce0b91bf6a405bdbc5a9e09e5a9b9afb8
joaovitorf4 <joaovitorfod5@gmail.com>
2024-09-06 09:37:44 -0300

    new styled component

---
+++ src/styled-components/H3Round.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const LabelTitle = styled.h3`
+    background-color: #001f75;
+    padding: .5em;
+    border-radius: 2em;
+    color: white;
+`;
+
+export default LabelTitle;
